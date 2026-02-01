3444a29dad90d1e6308488425cbc72984bd4f5f3
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-02-02 06:39:47 +0700

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,28 +1,14 @@
 import pandas as pd
 import streamlit as st
+import joblib
 from tensorflow.keras.models import load_model
-from sklearn.preprocessing import MinMaxScaler
-
 
 @st.cache_resource
 def load_all():
-    """
-    Load dataset, model LSTM, dan scaler.
-    Scaler di-fit satu kali menggunakan seluruh data historis.
-    """
-
-    # Load data
     df = pd.read_csv("data/data.csv")
     df["index"] = pd.to_datetime(df["index"])
 
-    # Load model
-    model = load_model(
-        "data/model_lstm.h5",
-        compile=False
-    )
-
-    # Inisialisasi & fit scaler
-    scaler = MinMaxScaler()
-    scaler.fit(df[["curah_hujan_mm_corrected"]])
+    model = load_model("data/model_lstm.h5", compile=False)
+    scaler = joblib.load("data/scaler_rainfall.pkl")
 
     return df, model, scaler
